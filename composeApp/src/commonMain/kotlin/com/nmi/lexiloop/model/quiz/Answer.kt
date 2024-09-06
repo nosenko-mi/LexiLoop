4a22989ca4157df1fc147ff7b1f82fd02c82a4ec
Mykola Nosenko <nosenkomi02@gmail.com>
2024-09-06 15:49:24 +0300

    update Answer

--- composeApp/src/commonMain/kotlin/com/nmi/lexiloop/model/quiz/Answer.kt
+++ composeApp/src/commonMain/kotlin/com/nmi/lexiloop/model/quiz/Answer.kt
@@ -1,12 +1,29 @@
 package com.nmi.lexiloop.model.quiz
 
+abstract class Answer {
 
-data class SimpleAnswerModel(
-    val text: String,
+    abstract val text: String
+
+    abstract fun isUserResponseCorrect(): Boolean
+}
+
+class SimpleAnswerModel(
+    override val text: String,
     val isCorrect: Boolean,
-)
+    var isChecked: Boolean = false,
+) : Answer() {
+    override fun isUserResponseCorrect(): Boolean {
+        return isChecked == isCorrect
+    }
 
-data class SequenceAnswerModel(
-    val text: String,
+}
+
+class SequenceAnswerModel(
+    override val text: String,
     val position: Int,
-)
+    var userSetPosition: Int = -1
+) : Answer() {
+    override fun isUserResponseCorrect(): Boolean {
+        return userSetPosition == position
+    }
+}
